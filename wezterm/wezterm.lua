0152084c63c2c8c866df69f57f2410d0546fbf04
hxsnaatdev <arizzzzzler@gmail.com>
2026-05-02 10:16:28 +0530

    feat : add tabline.wez
    modular view of the necessary components with native real estae
    managment of the tabline in nvim replicated for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,25 +1,57 @@
 local wezterm = require("wezterm")
-return {
-	adjust_window_size_when_changing_font_size = false,
-	-- color_scheme = "termnial.sexy",
-	color_scheme = "Catppuccin Mocha",
-	enable_tab_bar = false,
-	font_size = 18.0,
-	font = wezterm.font("Cascadia Code"),
-	macos_window_background_blur = 00,
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+local config = wezterm.config_builder()
 
-	window_background_opacity = 0.80,
-	window_decorations = "RESIZE",
-	keys = {
-		{
-			key = "q",
-			mods = "CTRL",
-			action = wezterm.action.ToggleFullScreen,
-		},
-		{
-			key = "'",
-			mods = "CTRL",
-			action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
-		},
+config.adjust_window_size_when_changing_font_size = false
+config.color_scheme = "Catppuccin Mocha"
+config.font_size = 18.0
+config.font = wezterm.font("Cascadia Code")
+config.macos_window_background_blur = 0
+config.window_background_opacity = 0.80
+config.window_decorations = "RESIZE"
+
+config.keys = {
+	{
+		key = "q",
+		mods = "CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+	{
+		key = "'",
+		mods = "CTRL",
+		action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
 	},
 }
+
+-- Tabline setup
+tabline.setup({
+	options = {
+		icons_enabled = true,
+		theme = "Catppuccin Mocha",
+		component_separators = {
+			left = wezterm.nerdfonts.pl_left_soft_divider,
+			right = wezterm.nerdfonts.pl_right_soft_divider,
+		},
+		tab_separators = {
+			left = wezterm.nerdfonts.pl_left_hard_divider,
+			right = wezterm.nerdfonts.pl_right_hard_divider,
+		},
+	},
+	sections = {
+		tabline_a = { "mode" },
+		tabline_b = { "workspace" },
+		tabline_c = { " " },
+		tab_active = {
+			"index",
+			{ "cwd", padding = { left = 0, right = 1 } },
+			{ "zoomed", padding = 0 },
+		},
+		tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
+		tabline_x = { "ram", "cpu" },
+		tabline_y = { "datetime", "battery" },
+	},
+})
+
+tabline.apply_to_config(config)
+
+return config
