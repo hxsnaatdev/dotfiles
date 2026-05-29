0c0b3e7645fe8610226952b63d82ae5451a624b5
hxsnaatdev <arizzzzzler@gmail.com>
2026-05-29 12:18:18 +0530

    feat: add sidekick

--- nvim/lua/plugins/sidekick.lua
+++ nvim/lua/plugins/sidekick.lua
@@ -7,43 +7,44 @@ return {
 					width = 0.4,
 				},
 			},
-			-- mux = {
-			--   backend = "tmux",
-			--   enabled = true,
-			-- },
-			tools = {
-				claude = { cmd = { "claude", "--dangerously-skip-permissions" } },
-				copilot = { cmd = { "copilot", "--allow-all-tools" } },
-				gemini = { cmd = { "gemini", "--yolo" } },
-				crush = { cmd = { "crush", "--yolo" } },
+			mux = {
+				backend = "zellij",
+				enabled = true,
+				-- },
+				tools = {
+					claude = { cmd = { "claude", "--dangerously-skip-permissions" } },
+					copilot = { cmd = { "copilot", "--allow-all-tools" } },
+					gemini = { cmd = { "gemini", "--yolo" } },
+					crush = { cmd = { "crush", "--yolo" } },
+				},
 			},
 		},
-	},
-	keys = {
-		{
-			"<D-r>",
-			function()
-				require("sidekick.cli").toggle("pi")
-			end,
-			desc = "Toggle AI window",
-			mode = { "n", "t", "i", "x" },
-		},
-		{
-			"<D-e>",
-			function()
-				local win = vim.api.nvim_get_current_win()
-				local width = vim.api.nvim_win_get_width(win)
-				local total = vim.o.columns
-				if width > total * 0.8 then
-					-- Restore to 40%
-					vim.api.nvim_win_set_width(win, math.floor(total * 0.4))
-				else
-					-- Maximize
-					vim.api.nvim_win_set_width(win, math.floor(total * 0.95))
-				end
-			end,
-			desc = "Toggle maximize window",
-			mode = { "n", "t", "i", "x" },
+		keys = {
+			{
+				"<D-r>",
+				function()
+					require("sidekick.cli").toggle("pi")
+				end,
+				desc = "Toggle AI window",
+				mode = { "n", "t", "i", "x" },
+			},
+			{
+				"<D-e>",
+				function()
+					local win = vim.api.nvim_get_current_win()
+					local width = vim.api.nvim_win_get_width(win)
+					local total = vim.o.columns
+					if width > total * 0.8 then
+						-- Restore to 40%
+						vim.api.nvim_win_set_width(win, math.floor(total * 0.4))
+					else
+						-- Maximize
+						vim.api.nvim_win_set_width(win, math.floor(total * 0.95))
+					end
+				end,
+				desc = "Toggle maximize window",
+				mode = { "n", "t", "i", "x" },
+			},
 		},
 	},
 }
