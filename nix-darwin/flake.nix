6c566f7d2cee3b214920d5243c34f87491883c15
hxsnaatdev <arizzzzzler@gmail.com>
2026-06-17 22:20:08 +0530

    fix: revert to head

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -1,77 +1,33 @@
 {
-  description = "my aarch64-darwin system flake";
+  description = "nix-darwin flake for reproducible builds";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-      }
-      ai-nixCfg = {
-          url = "github:DivitMittal/ai-nixCfg";
-          inputs.nixpkgs.follows = "nixpkgs";
-        };
+
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ 
-          pkgs.vim
-          pkgs.glow
-          pkgs.nushell
-          pkgs.carapace
+  outputs = { nix-darwin, home-manager, ... }:
+    let
+      darwinSystem = nix-darwin.lib.darwinSystem {
+        modules = [
+          ./hosts/darwin.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ariz = import ./home;
+          }
         ];
-      services.nix-daemon.enable = true;
-      nix.settings.experimental-features = "nix-command flakes";
-      programs.kitty.enable = true;  # default shell on catalina
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-      system.stateVersion = 4;
-      nixpkgs.hostPlatform = "aarch64-darwin";
-      security.pam.enableSudoTouchIdAuth = true;
-
-      users.users.ariz.home = "/Users/ariz/";
-      home-manager.backupFileExtension = "backup";
-      nix.configureBuildUsers = true;
-      nix.useDaemon = true;
-
-      system.defaults = {
-        dock.autohide = true;
-        dock.mru-spaces = false;
-        finder.AppleShowAllExtensions = true;
-        finder.FXPreferredViewStyle = "clmv";
-        loginwindow.LoginwindowText = "devops-toolbox";
-        screencapture.location = "~/Pictures/screenshots";
-        screensaver.askForPasswordDelay = 10;
       };
-
-      # Homebrew needs to be installed on its own!
-      homebrew.enable = true;
-      homebrew.casks = [
-	      "wireshark"
-          "google-chrome"
-      ];
-      homebrew.brews = [
-	      "imagemagick"
-      ];
-    };
-  in
-  {
-    darwinConfigurations."Hasnaats-MacBook-Air" = nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules = [ 
-	configuration
-        home-manager.darwinModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.ariz = import ./home.nix;
-        }
-      ];
+    in
+    {
+      darwinConfigurations."Hasnaats-MacBook-Air" = darwinSystem;
+      darwinConfigurations."ariz" = darwinSystem;
     };
 
     # Expose the package set, including overlays, for convenience.
